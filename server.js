c6e042f967005acfceb691079f3b39ea9531eca3
ogegbodegreat-creator <ogegbodegreat@gmail.com>
2026-08-25 14:50:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(express.json());
